56b6444c593cbe7e64ddf4bdb6ff052ed757ff5a
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-10 22:16:57 -0400

    add jbuilder and photos controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Photo.create(name: "Winter", width: 200, height: 150)
+Photo.create(name: "Family", width: 1024, height: 768)
